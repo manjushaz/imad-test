2417eda6b413db51dcdacb3c2bef8312c06a9ca2
Manjusha Z <manjushaz2012@gmail.com>
2017-09-13 12:32:43 +0530

    Halfway done with new feature.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+function newfeature() {
+   console.log("Started working on a new feature.");
+   console.log("Halfway done.");
+}
+
 function something() {
    console.log("I\'m something.");
 }
 
 console.log("Hello world! ");
 something();
+newfeature();
